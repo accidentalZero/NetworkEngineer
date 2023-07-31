ef1fb080ecdad1084922d41653e87258546d9fd3
accidentalZero <accidental0zero@gmail.com>
2023-07-31 22:39:31 +0300

    Выполнил задание 4.8

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -26,3 +26,9 @@
 """
 
 ip = "192.168.3.1"
+ip = [int(x) for x in ip.split('.')]
+ip_template = '''
+{0:<8}  {1:<8}  {2:<8}  {3:<8}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}
+'''
+print(ip_template.format(*ip))
