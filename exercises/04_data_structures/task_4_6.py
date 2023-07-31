dde43a9779ea831b8bf6706fcad8cc3a1381a6c5
accidentalZero <accidental0zero@gmail.com>
2023-07-31 22:36:56 +0300

    Выполнил задание 4.6

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -21,6 +21,9 @@ ospf_route. Значения из строки ospf_route надо получи�
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+ospf_route = ospf_route.replace('[', '').replace(']', '').replace(',', '')
+values = ospf_route.split()
+values.remove('via')
 template = """
 Prefix                {}
 AD/Metric             {}
@@ -28,3 +31,4 @@ Next-Hop              {}
 Last update           {}
 Outbound Interface    {}
 """
+print(template.format(*values))
